64b484d22365f5dd5f9f352b71fae52dd7b7b6fb
Nicolas Huber <nicolas.huber2@uzh.ch>
2023-06-13 17:12:56 +0200

    Prism themes

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,37 @@ import Resume from "./components/Resume.jsx";
 import Projects from "./components/Projects.jsx";
 import Typewriter from "./components/Typewriter.jsx";
 import PersonalTimeline from "./components/PersonalTimeline.jsx";
+import ProgrammingCode from "./components/ProgrammingCode.jsx";
 
 function App() {
     const about = ['Aspiring Code Alchemist','Intermediate Backend Maestro', 'Floorball Enthusiast', 'IT Security Practitioner'];
+    const javaCode = `public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}`;
+
+    const latexCode = `\\documentclass{article}
+\\begin{document}
+Hello, \\LaTeX!
+\\end{document}`;
+
+    const phpCode = `<?php
+echo "Hello, World!";
+?>`;
+
+    const pythonCode = `print("Hello, World!")`;
+
+    const sqlCode = `SELECT * FROM users`;
+    const JSCode = `const App = props => {
+  return (
+    <div>
+      <h1> Prism JS </h1>
+      <div>Awesome Syntax Highlighter</div>
+    </div>
+  );
+};
+`;
   return (
     <>
         <Navbar></Navbar>
@@ -25,6 +53,12 @@ function App() {
         <Skills></Skills>
         <Resume></Resume>
         <Projects></Projects>
+        <ProgrammingCode language="javascript" code={JSCode} />
+        <ProgrammingCode language="java" code={javaCode} />
+        <ProgrammingCode language="latex" code={latexCode} />
+        <ProgrammingCode language="php" code={phpCode} />
+        <ProgrammingCode language="python" code={pythonCode} />
+        <ProgrammingCode language="sql" code={sqlCode} />
         <Typewriter sentences={about}></Typewriter>
 
     </>
---
+++ src/components/ProgrammingCode.jsx
@@ -0,0 +1,28 @@
+import { useEffect } from 'react';
+import Prism from 'prismjs';
+// import 'prismjs/themes/prism-dark.css'; // prism original
+import '../../themes/prism-shades-of-purple.css';
+import 'prismjs/components/prism-python.js'
+import 'prismjs/components/prism-sql.js'
+import 'prismjs/components/prism-latex.js'
+import 'prismjs/components/prism-java.js'
+import 'prismjs/components/prism-php'
+import 'prismjs/components/prism-markup-templating'
+
+function ProgrammingCode({language, code}) {
+
+    useEffect(() => {
+        Prism.highlightAll();
+    }, []);
+
+    return (
+        <div className="Code">
+            <h2> Code Syntax Block {language}</h2>
+            <pre>
+                <code className={`language-${language}`}>{code}</code>
+            </pre>
+        </div>
+    );
+}
+
+export default ProgrammingCode
