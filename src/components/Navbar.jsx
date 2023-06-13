c4c1a69e93770eb829a8fd1d6d58afd6c02d7811
Nicolas Huber <nicolas.huber2@uzh.ch>
2023-06-13 15:59:50 +0200

    Icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,23 @@
 import Button from '@mui/material/Button';
+import HouseIcon from '@mui/icons-material/House';
+import PersonIcon from '@mui/icons-material/Person';
+import SchoolIcon from '@mui/icons-material/School';
+import TerminalIcon from '@mui/icons-material/Terminal';
+import MergeIcon from '@mui/icons-material/Merge';
+import SendIcon from '@mui/icons-material/Send';
 function Navbar() {
 
     return (
         <>
-            <div>Navbar</div>
-            <Button>Hello</Button>
-
+            <div id="navbar-container">
+                <Button>Hello</Button>
+                <HouseIcon></HouseIcon>
+                <PersonIcon></PersonIcon>
+                <SchoolIcon></SchoolIcon>
+                <TerminalIcon></TerminalIcon>
+                <MergeIcon></MergeIcon>
+                <SendIcon></SendIcon>
+            </div>
         </>
     )
 }
