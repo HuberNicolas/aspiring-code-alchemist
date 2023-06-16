1c6292f8eb326ab7ad0c3363ad7274bc4d38c573
Nicolas Huber <nicolas.huber2@uzh.ch>
2023-06-16 20:38:12 +0200

    Basic Parallax

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,12 @@ import Projects from "./components/Projects.jsx";
 import Typewriter from "./components/Typewriter.jsx";
 import PersonalTimeline from "./components/PersonalTimeline.jsx";
 import ProgrammingCode from "./components/ProgrammingCode.jsx";
+import { Parallax, ParallaxLayer } from '@react-spring/parallax'
+import {useRef} from "react";
 
 function App() {
+    const ref = useRef();
+
     const about = ['Aspiring Code Alchemist','Intermediate Backend Maestro', 'Floorball Enthusiast', 'IT Security Practitioner'];
     const javaCode = `public class HelloWorld {
     public static void main(String[] args) {
@@ -40,27 +44,59 @@ echo "Hello, World!";
   );
 };
 `;
-  return (
+
+    return (
     <>
         <Navbar></Navbar>
-        <Jokes></Jokes>
-        <Hero></Hero>
+        <Parallax pages={4} ref={ref}>
+            <ParallaxLayer>
+                <h1>Home</h1>
+                <Jokes></Jokes>
+                <Hero></Hero>
+                <Typewriter sentences={about}></Typewriter>
 
-        <Education></Education>
-        <PersonalTimeline></PersonalTimeline>
-        <Skills></Skills>
-        <Contact></Contact>
-        <Skills></Skills>
-        <Resume></Resume>
-        <Projects></Projects>
-        <ProgrammingCode language="javascript" code={JSCode} />
-        <ProgrammingCode language="java" code={javaCode} />
-        <ProgrammingCode language="latex" code={latexCode} />
-        <ProgrammingCode language="php" code={phpCode} />
-        <ProgrammingCode language="python" code={pythonCode} />
-        <ProgrammingCode language="sql" code={sqlCode} />
-        <Typewriter sentences={about}></Typewriter>
+            </ParallaxLayer>
+            <ParallaxLayer
+                offset={1}
+                onClick={() => ref.current.scrollTo(3)}
+            >
+                <h1>Education</h1>
+                <Education></Education>
+                <PersonalTimeline></PersonalTimeline>
+                <Resume></Resume>
 
+            </ParallaxLayer>
+            <ParallaxLayer
+                offset={2}
+                onClick={() => ref.current.scrollTo(1)}
+                speed={0.5}
+            >
+                <h1>Skills</h1>
+                <Skills></Skills>
+                <ProgrammingCode language="javascript" code={JSCode} />
+                <ProgrammingCode language="java" code={javaCode} />
+                <ProgrammingCode language="latex" code={latexCode} />
+                <ProgrammingCode language="php" code={phpCode} />
+                <ProgrammingCode language="python" code={pythonCode} />
+                <ProgrammingCode language="sql" code={sqlCode} />
+
+
+            </ParallaxLayer>
+            <ParallaxLayer offset={3} speed={0.75}>
+                <h1>Projects</h1>
+                <Projects></Projects>
+
+            </ParallaxLayer>
+            <ParallaxLayer offset={4} speed={0.3}>
+                <h1>Contact</h1>
+                <Contact></Contact>
+
+            </ParallaxLayer>
+
+
+
+
+        </Parallax>
     </>
   )
 }
