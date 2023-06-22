7f41c3edfae6bdae0cda7d1b0cce96dba7cdd93a
Nicolas Huber <nicolas.huber2@uzh.ch>
2023-06-22 14:27:54 +0200

    Typewriting Code except PHP

--- src/components/ProgrammingCode.jsx
+++ src/components/ProgrammingCode.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import Prism from 'prismjs';
 // import 'prismjs/themes/prism-dark.css'; // prism original
 import '../../themes/prism-shades-of-purple.css';
@@ -6,23 +6,43 @@ import 'prismjs/components/prism-python.js'
 import 'prismjs/components/prism-sql.js'
 import 'prismjs/components/prism-latex.js'
 import 'prismjs/components/prism-java.js'
-import 'prismjs/components/prism-php'
-import 'prismjs/components/prism-markup-templating'
+import 'prismjs/components/prism-php.js'
+import 'prismjs/components/prism-markup-templating.js'
+import Typed from "typed.js";
 
-function ProgrammingCode({language, code}) {
+function ProgrammingCode({ language, code }) {
+    const codeRef = useRef(null);
+    const typedRef = useRef(null);
 
     useEffect(() => {
-        Prism.highlightAll();
-    }, []);
+        const options = {
+            strings: [code],
+            typeSpeed: 50,
+            showCursor: false,
+            startDelay: 1000,
+            smartBackspace: true,
+            onComplete: () => {
+                Prism.highlightAll();
+            },
+        };
+
+        typedRef.current = new Typed(codeRef.current, options);
+
+        return () => {
+            if (typedRef.current) {
+                typedRef.current.destroy();
+            }
+        };
+    }, [code]);
 
     return (
         <div className="Code">
-            <h2> Code Syntax Block {language}</h2>
+            <h2>Code Syntax Block {language}</h2>
             <pre>
-                <code className={`language-${language}`}>{code}</code>
-            </pre>
+        <code ref={codeRef} className={`language-${language} preformatted`}></code>
+      </pre>
         </div>
     );
 }
 
-export default ProgrammingCode
+export default ProgrammingCode;
