54cf6d0a8662d95877ea59183bd10c2d80efeee9
Nicolas Huber <nicolas.huber2@uzh.ch>
2023-06-25 09:50:00 +0200

    Jokes

--- src/components/Jokes.jsx
+++ src/components/Jokes.jsx
@@ -1,10 +1,34 @@
-function Jokes() {
+import { useState, useEffect } from 'react';
+import jokes from '../assets/jokes.json';
+
+const Jokes = () => {
+    const [randomJoke, setRandomJoke] = useState('');
+    const [randomAuthor, setRandomAuthor] = useState('');
+
+    useEffect(() => {
+        loadJokes();
+    }, []);
+
+    const loadJokes = () => {
+        const jokesArray = jokes.jokes_quotes;
+        getRandomJoke(jokesArray);
+    };
+
+    const getRandomJoke = (jokesArray) => {
+        const randomIndex = Math.floor(Math.random() * jokesArray.length);
+        setRandomJoke(jokesArray[randomIndex].quote);
+        setRandomAuthor(jokesArray[randomIndex].author);
+    };
 
     return (
         <>
-            <div>Jokes</div>
+            <div>Joke</div>
+            <div>
+                <p>{randomJoke}</p>
+                <p>- {randomAuthor}</p>
+            </div>
         </>
-    )
-}
+    );
+};
 
-export default Jokes
+export default Jokes;
