74a1963f84bc0059daf483505e5e596a0295e508
Nicolas Huber <nicolas.huber2@uzh.ch>
2023-06-26 16:41:28 +0200

    Deployment

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), ["prismjs", {
-    "languages": ["java", "latex", "php", "python", "sql", "javascript"],
-    "plugins": ["line-numbers"],
-    "theme": "twilight",
-    "css": true
-  }]],
+  plugins: [react()],
   base: '/aspiring-code-alchemist/',
 })
