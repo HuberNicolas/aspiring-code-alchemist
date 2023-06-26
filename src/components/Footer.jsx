c2eb6d8d0ffb70a43af62e72909f5df56c6eb284
Nicolas Huber <nicolas.huber2@uzh.ch>
2023-06-26 09:56:57 +0200

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,6 +7,8 @@ import PersonIcon from '@mui/icons-material/Person';
 import TerminalIcon from '@mui/icons-material/Terminal';
 import MergeIcon from '@mui/icons-material/Merge';
 import SendIcon from '@mui/icons-material/Send';
+import reactLogo from '../assets/react.svg'
+import materialLogo from '../assets/material.svg'
 
 import './Footer.scss';
 import {Dna} from "react-loader-spinner";
@@ -31,17 +33,25 @@ function Footer() {
                     padding: '20px',
                 }}
             >
-                <Dna
-                    visible={true}
-                    height="80"
-                    width="80"
-                    ariaLabel="dna-loading"
-                    wrapperStyle={{}}
-                    wrapperClass="dna-wrapper"
-                />
                 <Typography variant="h7" align="center">
                     React based & made with ❤️ (aka. Material UI).
                 </Typography>
+                <div>
+                    <a href="https://react.dev" target="_blank">
+                        <img src={reactLogo} className="logo react" alt="React logo" />
+                    </a>
+                    <Dna
+                        visible={true}
+                        height="80"
+                        width="80"
+                        ariaLabel="dna-loading"
+                        wrapperStyle={{}}
+                        wrapperClass="dna-wrapper"
+                    />
+                    <a href="https://mui.com/" target="_blank">
+                        <img src={materialLogo} className="logo material" alt="Material logo" />
+                    </a>
+                </div>
             </div>
         </div>
     );
