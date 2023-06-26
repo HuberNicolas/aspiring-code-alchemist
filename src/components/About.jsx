10ef6cd8910e6f93b6155c0eca317dfb86362f32
Nicolas Huber <nicolas.huber2@uzh.ch>
2023-06-26 14:45:43 +0200

    Layout

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,35 @@
 import PersonalInfoCard from "./PersonalInfoCard.jsx";
+import profileImage from "../assets/profile.jpg";
+import {Avatar, Grid} from "@mui/material";
 
 function About() {
 
     return (
         <>
-            <PersonalInfoCard></PersonalInfoCard>
+            <Grid container spacing={2} sx={{ padding: '20px' }}>
+                <Grid item xs={12} sm={6}>
+                    <PersonalInfoCard />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    <Avatar
+                        alt="Nicolas Huber"
+                        src={profileImage}
+                        sx={{
+                            marginLeft: 'auto',
+                            marginRight: 'auto',
+                            height: '90%',
+                            width: '50%',
+                            borderTopLeftRadius: '100%',
+                            borderTopRightRadius: '100%',
+                            borderBottomLeftRadius: '100%',
+                            borderBottomRightRadius: '100%',
+                            border: '4px solid #009688',
+                            boxSizing: 'border-box',
+                        }}
+                    />
+
+                </Grid>
+            </Grid>
         </>
     )
 }
--- src/components/PersonalInfoCard.jsx
+++ src/components/PersonalInfoCard.jsx
@@ -11,35 +11,35 @@ const PersonalInfoCard = () => {
                 <Typography variant="h6" component="h2">
                     Hobbies 🌟
                 </Typography>
-                <Typography>
+                <Typography sx={{ marginBottom: '2rem' }}>
                     Coding, Floorball and Bodyweight Fitness, Psychology, Data Adventurer on the Rise, IT Security
                 </Typography>
 
                 <Typography variant="h6" component="h2">
                     Strengths 🔥
                 </Typography>
-                <Typography>
+                <Typography sx={{ marginBottom: '2rem' }}>
                     Backend Development with Symfony, day-to-day organizing skills, data-savvy, strong Communicator
                 </Typography>
 
                 <Typography variant="h6" component="h2">
                     Character Traits 🗝️
                 </Typography>
-                <Typography>
+                <Typography sx={{ marginBottom: '2rem' }}>
                     Outgoing, organised and proactive, curiosity driven, sometimes lost in details
                 </Typography>
 
                 <Typography variant="h6" component="h2">
                     Favorites 🎯
                 </Typography>
-                <Typography>
-                    Summer, roof terraces, Fight Club, Hearts of Iron 4, Lasagne, Byron Bay and Campfires
+                <Typography sx={{ marginBottom: '2rem' }}>
+                    Summer, Roof terraces, Hearts of Iron 4, Lasagne, Byron Bay and Campfires
                 </Typography>
 
                 <Typography variant="h6" component="h2">
                     Le me in a nutshell 🌰
                 </Typography>
-                <Typography>
+                <Typography sx={{ marginBottom: '2rem' }}>
                     Mastering informatics & data science at UZH while moonlighting at jkweb, I navigate the backend realm, ensure smooth app testing, and occasionally sprinkle knowledge on apprentice minds at BBC Bern.
                 </Typography>
             </CardContent>
